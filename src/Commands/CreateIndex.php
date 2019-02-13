d3b6a9d007674b1247411b13f69ed07981ac2eca
VBoss <vcevela@gmail.com>
2019-02-14 00:09:34 +0100

    Fix when specified unknown entity name, dont run all of them

--- src/Commands/CreateIndex.php
+++ src/Commands/CreateIndex.php
@@ -34,11 +34,17 @@ class CreateIndex extends \Symfony\Component\Console\Command\Command
 			->setName('spameri:elastic:create-index')
 			->setDescription('Creates index and puts mapping for entity/ies.')
 			->addArgument('entityName', \Symfony\Component\Console\Input\InputArgument::OPTIONAL)
-			->addOption('force', 'f', NULL, 'Warning this deletes your data! Forces now used index to be deleted before new index is created and mapping set.')
+			->addOption(
+				'force', 'f', NULL,
+				'Warning this deletes your data! Forces now used index to be deleted before new index is created and mapping set.'
+			)
 		;
 	}
 
 
+	/**
+	 * @throws \Spameri\Elastic\Exception\ElasticSearch
+	 */
 	protected function execute(
 		\Symfony\Component\Console\Input\InputInterface $input
 		, \Symfony\Component\Console\Output\OutputInterface $output
@@ -48,28 +54,36 @@ class CreateIndex extends \Symfony\Component\Console\Command\Command
 		$entityName = $input->getArgument('entityName');
 		$forcedDelete = $input->getOption('force');
 
+		if ($entityName) {
+			if ( ! isset($this->entities[$entityName])) {
+				$output->writeln('Provided entity name ' . $entityName . ' is not found in configuration.');
 
-		if ($entityName && isset($this->entities[$entityName])) {
-			if ($forcedDelete) {
-				$this->elasticMapper->deleteIndex($this->entities[$entityName]['index']);
-			}
-			try {
-				$this->elasticMapper->createIndex($this->entities[$entityName]);
+			} else {
+				if ($forcedDelete) {
+					$this->elasticMapper->deleteIndex($this->entities[$entityName]['index']);
+					$output->writeln('Index ' . $this->entities[$entityName]['index'] . ' deleted.');
+				}
+				try {
+					$this->elasticMapper->createIndex($this->entities[$entityName]);
+					$output->writeln('Index ' . $this->entities[$entityName]['index'] . ' created.');
 
-			} catch (\Spameri\Elastic\Exception\ElasticSearchException $exception) {
-				$output->write($exception->getMessage());
+				} catch (\Spameri\Elastic\Exception\ElasticSearchException $exception) {
+					$output->writeln($exception->getMessage());
+				}
 			}
 
 		} else {
 			foreach ($this->entities as $entity) {
 				if ($forcedDelete) {
 					$this->elasticMapper->deleteIndex($entity['index']);
+					$output->writeln('Index ' . $this->entities[$entityName]['index'] . ' deleted.');
 				}
 				try {
 					$this->elasticMapper->createIndex($entity);
+					$output->writeln('Index ' . $this->entities[$entityName]['index'] . ' created.');
 
 				} catch (\Spameri\Elastic\Exception\ElasticSearchException $exception) {
-					$output->write($exception->getMessage());
+					$output->writeln($exception->getMessage());
 				}
 			}
 		}
