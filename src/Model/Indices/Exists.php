6f41e81bdf23ca2456c640a546b963e64fcdc8b8
VBoss <vcevela@gmail.com>
2021-05-18 12:48:42 +0200

    Added indices and index endpoint handlers Indices - Exists to check if index exists Indices - GetFieldMapping to get mapping for type Insert - to insert raw data to elastic

---
+++ src/Model/Indices/Exists.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types = 1);
+
+namespace Spameri\Elastic\Model\Indices;
+
+class Exists
+{
+
+	private \Spameri\Elastic\ClientProvider $clientProvider;
+
+
+	public function __construct(
+		\Spameri\Elastic\ClientProvider $clientProvider
+	)
+	{
+		$this->clientProvider = $clientProvider;
+	}
+
+
+	public function execute(
+		string $index
+	): bool
+	{
+		try {
+			return $this->clientProvider->client()->indices()->exists(
+				(
+					new \Spameri\ElasticQuery\Document(
+						$index
+					)
+				)->toArray()
+			)
+			;
+
+		} catch (\Elasticsearch\Common\Exceptions\ElasticsearchException $exception) {
+			throw new \Spameri\Elastic\Exception\ElasticSearch($exception->getMessage());
+		}
+	}
+
+}
