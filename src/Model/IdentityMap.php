0594eba2af4b2424130e362f5a44a6fb5f4fb87a
Spamer <vcevela@gmail.com>
2025-02-22 23:05:09 +0100

    update docblok

--- src/Model/IdentityMap.php
+++ src/Model/IdentityMap.php
@@ -21,9 +21,9 @@ class IdentityMap
 	public array $creatingEntityList = [];
 
 	/**
-	 * entity that is missing, id, propertyName, entity that has missing entity in property
+	 * entity that is missing, its id, propertyName where it is missing, entityId, entityClass that has missing entity in property
 	 *
-	 * @var array<class-string, array<string, array<string, \Spameri\Elastic\Entity\AbstractElasticEntity>>>
+	 * @var array<class-string, array<string, array<string, array<string, class-string>>>>
 	 */
 	public array $uninitializedEntityList = [];
 
