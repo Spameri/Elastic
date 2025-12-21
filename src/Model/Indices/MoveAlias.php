20abf2a23142c5f652ebd092abc2901ca6de470d
Spamer <vcevela@gmail.com>
2025-12-22 00:25:07 +0100

    refactor: update alias handling to use updateAliases method

--- src/Model/Indices/MoveAlias.php
+++ src/Model/Indices/MoveAlias.php
@@ -18,30 +18,25 @@ readonly class MoveAlias
 	public function execute(string $alias, string $indexFrom, string $indexTo): array
 	{
 		try {
-			return $this->clientProvider->client()->indices()->putAlias(
-				(
-				new \Spameri\ElasticQuery\Document(
-					$indexFrom,
-					new \Spameri\ElasticQuery\Document\Body\Plain(
-						[
-							'actions' => [
+			return $this->clientProvider->client()->indices()->updateAliases(
+				[
+					'body' => [
+						'actions' => [
+							[
 								'remove' => [
 									'index' => $indexFrom,
 									'alias' => $alias,
 								],
+							],
+							[
 								'add' => [
 									'index' => $indexTo,
 									'alias' => $alias,
 								],
 							],
 						],
-					),
-					null,
-					[
-						'name' => $indexTo,
 					],
-				)
-				)->toArray(),
+				],
 			)->asArray()
 				;
 
