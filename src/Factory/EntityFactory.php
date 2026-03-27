a2494a285f5fea79866cd91679c6bcf53b0f001c
Spamer <vcevela@gmail.com>
2026-03-27 16:30:45 +0100

    Revert "fix: handle DocumentNotFound for nullable ElasticEntity properties"
    This reverts commit f55340b17fd28c97a122b0f76018cfdd2fd06e1d.

--- src/Factory/EntityFactory.php
+++ src/Factory/EntityFactory.php
@@ -243,19 +243,10 @@ readonly class EntityFactory implements \Spameri\Elastic\Factory\EntityFactoryIn
 						$this->identityMap->uninitializedEntityList[$propertyTypeName][$value][$property->getName()][$hit->id()] = $class;
 
 					} else {
-						try {
-							$propertyValue = $entityManager->find(
-								id: $value,
-								class: $propertyTypeName,
-							);
-						} catch (\Spameri\Elastic\Exception\DocumentNotFound $e) {
-							if ($reflectionPropertyType->allowsNull()) {
-								$propertyValue = null;
-								$setNull = true;
-							} else {
-								throw $e;
-							}
-						}
+						$propertyValue = $entityManager->find(
+							id: $value,
+							class: $propertyTypeName,
+						);
 					}
 
 				} elseif ($this->container->getByType($propertyTypeName, false) !== null) {
